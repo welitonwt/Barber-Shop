24f6c43303dcb0cd5460b208de26147f21b18b10
weliton <weliton.ww@hotmail.com>
2025-02-21 17:31:34 -0300

    list hours

---
+++ src/modules/schedules/load.js
@@ -0,0 +1,7 @@
+import { hoursLoad } from "../form/hours-load.js"
+
+const selectedDate = document.getElementById("date")
+export  function schedulesDay() {
+  const date = selectedDate.value
+  hoursLoad({date})
+}
